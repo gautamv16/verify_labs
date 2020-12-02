e732e5b5bcd3ec6ed8c00e389dacb1b31a71fb49
veer <veer88virk@gmail.com>
2020-12-02 15:43:58 +0530

    changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,18 +3,22 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header">Search Report</div>
+        <div class="searchShipment flex-column">
+            <div class="d-flex justify-content-center align-items-center searchuprLogo mb-20">
+                <img src="{{ asset('admin/assets/img/lab-verification-logo.png') }}" height="60"> 
+                <span>Search Shipment</span>
+            </div>
 
-                <div class="card-body">
-                    <form method="post" action="{{route('searchreport')}}">
-                    @csrf
-                       <input type="text" placeholder="Enter FINS No" name="fins_number"  class="form-control" />
-               </form>
-                </div>
+            <div class="relative">
+                <form method="post" action="{{route('searchreport')}}">
+                @csrf
+                    <i class="fa fa-search"></i>
+                   <input type="text" placeholder="Enter FINS No" name="fins_number"  class="form-control" />
+                   <button class="btn btn-search">Search</button>
+                </form>
             </div>
         </div>
+        
     </div>
 </div>
 @endsection
