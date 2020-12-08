64af2ccbbf3acc9f081a72bdade7c2e9554f10fd
gautam <neeraj0710thakur@gmail.com>
2020-12-08 12:57:58 +0530

    added importer exporter detail

--- resources/views/admin/labs/index.blade.php
+++ resources/views/admin/labs/index.blade.php
@@ -13,7 +13,7 @@
     </div>
 </div>
 <div class="container-fluid">
-    <div class="card mb-4 border-0">
+    <div class=" mb-4 border-0">
         <div>
             <div class="table-responsive">
                 <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
